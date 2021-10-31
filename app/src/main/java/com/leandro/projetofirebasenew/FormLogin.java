d74cedc812958a9467dab75a285772f28bd088a7
Leandro <felixbassoromanew@gmail.com>
2021-10-31 07:06:46 -0300

    Start Commit!

---
+++ app/src/main/java/com/leandro/projetofirebasenew/FormLogin.java
@@ -0,0 +1,15 @@
+package com.leandro.projetofirebasenew;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+    }
+}
